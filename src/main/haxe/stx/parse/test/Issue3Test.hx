f73e6c87d080e42b12be4aa841e0bb987b4d9dbd
0b1kn00b <polysemantic@gmail.com>
2021-10-15 13:11:17 +0200

    added haxe.json

---
+++ src/main/haxe/stx/parse/test/Issue3Test.hx
@@ -0,0 +1,40 @@
+package stx.parse.test;
+
+class Issue3Test extends TestCase{
+  public function _testSomething(){
+    final value   = 'a'.reader();
+    final parse   = Parser.Something();
+    final result  = parse.toFletcher().fudge(value);
+    //__.log().debug(_ -> _.pure(result));
+  }
+  public function _testOr(){
+    final value   = 'b'.reader();
+    final parse   = Parser.Identifier('a').or(Parser.Identifier('b'));
+    final result  = parse.toFletcher().fudge(value);
+    //trace(result);
+  }
+  public function _testMany(){
+    final value   = "aa".reader();
+    final parse   = Parser.Many(Parser.Something());
+    final result  = parse.toFletcher().fudge(value);
+    trace(result);
+  }
+  public function _testMemo(){
+    final value   = "aa".reader();
+    final parse   = Parser.Something().memo();
+    final result  = parse.toFletcher().fudge(value);
+    trace(result);
+  }
+  public function _testThen(){
+    final value   = "aa".reader();
+    final parse   = Parser.Something().then(_ -> Noise);
+    final result  = parse.toFletcher().fudge(value);
+    trace(result);
+  }
+  public function _testAnd(){
+    final value   = "aa".reader();
+    final parse   = Parser.Something().and(Parser.Something());
+    final result  = parse.toFletcher().fudge(value);
+    trace(result);
+  }
+}
